1018586cddda26788e43b501b96458db9b06b41e
Prathamputhran06 <prathamputhran45@gmail.com>
2023-01-28 10:36:46 +0530

    fizz

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+#print fizz if divisible by3 similarly other two
+for i in range(1,100):
+    if i%3==0 and i%5==0:
+        print("FizzBuzz") 
+    elif i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
